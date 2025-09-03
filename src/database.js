9cf7c1ecbaac9a26c69e86375f6e4e5e674e06f0
viniciussousa16 <85960539+viniciussousa16@users.noreply.github.com>
2025-09-03 08:24:05 -0300

    parametros por id

--- src/database.js
+++ src/database.js
@@ -5,27 +5,27 @@ const databasePath = new URL('../db.json', import.meta.url)
 export class Database {
     #database = {}
 
-    constructor(){
-        fs.readFile(databasePath, 'utf-8').then(data =>{
+    constructor() {
+        fs.readFile(databasePath, 'utf-8').then(data => {
             this.#database = JSON.parse(data)
         })
-        .catch(() => {
-            this.#persist()
-        })
+            .catch(() => {
+                this.#persist()
+            })
     }
 
-    #persist(){
+    #persist() {
         fs.writeFile(databasePath, JSON.stringify(this.#database))
     }
 
-    select(table){
+    select(table) {
         const data = this.#database[table] ?? []
 
         return data
     }
 
     insert(table, data) {
-        if (Array.isArray(this.#database[table])){
+        if (Array.isArray(this.#database[table])) {
             this.#database[table].push(data)
         } else {
             this.#database[table] = [data]
@@ -33,6 +33,15 @@ export class Database {
 
         this.#persist();
 
-        return  data
+        return data
+    }
+
+    delete(table, id) {
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+        if (rowIndex > -1) {
+            this.#database[table].splice(rowIndex, 1)
+            this.#persist()
+        }
     }
 }
--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,7 +1,7 @@
 // /users/:id
 export function buildRoutePath(path){
     const routeParametersRegex = /:([a-zA-Z]+)/g
-    const pathWithParams = path.replaceAll(routeParametersRegex, '([a-z0-9\-_]+)')
+    const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
     const pathRegex = new RegExp(`^${pathWithParams}`)
     
     return pathRegex
